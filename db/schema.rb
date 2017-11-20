736efef84f2bed935af626d59ae91c7cc42e74e4
Constantin Guidon <constantin.guidon@gmail.com>
2017-11-20 03:58:49 +0100

    [fix] tests

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,32 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_171_117_085_753) do
+ActiveRecord::Schema.define(version: 20171117085753) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'flame_paths', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "flame_paths", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'polls', force: :cascade do |t|
-    t.string 'email'
-    t.string 'fb_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'flame_path_id'
-    t.index ['flame_path_id'], name: 'index_polls_on_flame_path_id'
+  create_table "polls", force: :cascade do |t|
+    t.string "email"
+    t.string "fb_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "flame_path_id"
+    t.index ["flame_path_id"], name: "index_polls_on_flame_path_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'polls', 'flame_paths'
+  add_foreign_key "polls", "flame_paths"
 end
