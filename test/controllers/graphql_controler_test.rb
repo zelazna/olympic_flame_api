4525ca7bd14175dcac38dc70eadaeaf378acfc7e
zelazna <constantin.guidon@gmail.com>
2017-12-19 14:14:13 +0100

    [tests] improve coverage

--- test/controllers/graphql_controler_test.rb
+++ test/controllers/graphql_controler_test.rb
@@ -2,13 +2,29 @@
 
 require 'test_helper'
 
-class GraphqlControllerTest < ActionController::TestCase
+class GraphqlControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = User.first
+    @token = JsonWebToken.encode(id: @user.id)
   end
 
   test 'should execute the graphql query' do
-    post :execute, params: { query: '{ __schema }' }, format: :json
+    post graphql_url, params: { query: '{ __schema }' }, as: :json
 
     assert_response :success
+  end
+
+  test 'should store a user in context' do
+    post graphql_url, params: { query: '{ __schema }' }, headers: { 'Authorization': "Bearer #{@token}" }, as: :json
+  end
+
+  test 'should raise an error on Unexpected parameters' do
+    assert_raise ArgumentError do
+      post graphql_url, params: { variables: 123 }, as: :text
+    end
+  end
+
+  test 'should return a empty hash' do
+    post graphql_url, params: { variables: { 'test': 'retest' } }
   end
 end
