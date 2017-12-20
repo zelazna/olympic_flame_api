5df677ffbe797415168b9204e6edcf69cca6b1e4
Constantin Guidon <constantin.guidon@gmail.com>
2017-12-20 23:57:26 +0100

    improving tests

--- test/graphql/resolvers/create_torch_vote_test.rb
+++ test/graphql/resolvers/create_torch_vote_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 module Resolvers
   class CreateTorchVoteTest < ActiveSupport::TestCase
-    def perform(args = { torchId: 1 }, ctx = { current_user: @user })
+    def perform(args, ctx)
       Resolvers::CreateTorchVote.new.call(nil, args, ctx)
     end
 
@@ -13,9 +13,15 @@ module Resolvers
     end
 
     test 'creating new torch vote' do
-      vote = perform
+      vote = perform({ torchId: 1 }, current_user: @user)
 
       assert vote.persisted?
+    end
+
+    test 'must raise an exception if context doesnt hold user' do
+      assert_raise GraphQL::ExecutionError do
+        perform({ torchId: 1 }, current_user: nil)
+      end
     end
   end
 end
