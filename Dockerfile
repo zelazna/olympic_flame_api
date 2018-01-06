909a81c728460db579698fe35ac949e9b2bbf679
Constantin Guidon <constantin.guidon@gmail.com>
2018-01-06 16:43:26 +0100

    [feature] docker

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM ruby:2.4.1
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+RUN mkdir /olympic_flame_api
+WORKDIR /olympic_flame_api
+COPY Gemfile /olympic_flame_api/Gemfile
+COPY Gemfile.lock /olympic_flame_api/Gemfile.lock
+RUN bundle install
+COPY . /olympic_flame_api
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,69 +10,70 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_103_080_055) do
+ActiveRecord::Schema.define(version: 20180103080055) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'events', force: :cascade do |t|
-    t.date 'date'
-    t.string 'title'
-    t.string 'location'
-    t.string 'description'
-    t.text 'images_url', default: [], array: true
-    t.bigint 'flame_path_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['flame_path_id'], name: 'index_torches_on_flame_path_id'
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.string "title"
+    t.string "location"
+    t.string "description"
+    t.text "images_url", default: [], array: true
+    t.bigint "flame_path_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flame_path_id"], name: "index_events_on_flame_path_id"
   end
 
-  create_table 'flame_paths', force: :cascade do |t|
-    t.string 'name'
-    t.string 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "flame_paths", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'points', force: :cascade do |t|
-    t.float 'lat'
-    t.float 'long'
-    t.bigint 'flame_path_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['flame_path_id'], name: 'index_points_on_flame_path_id'
+  create_table "points", force: :cascade do |t|
+    t.float "lat"
+    t.float "long"
+    t.bigint "flame_path_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flame_path_id"], name: "index_points_on_flame_path_id"
   end
 
-  create_table 'torch_votes', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'torch_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['torch_id'], name: 'index_torch_votes_on_torch_id'
-    t.index ['user_id'], name: 'index_torch_votes_on_user_id'
+  create_table "torch_votes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "torch_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["torch_id"], name: "index_torch_votes_on_torch_id"
+    t.index ["user_id"], name: "index_torch_votes_on_user_id"
   end
 
-  create_table 'torches', force: :cascade do |t|
-    t.string 'name'
-    t.string 'description'
-    t.string 'image_url'
-    t.bigint 'flame_path_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['flame_path_id'], name: 'index_torches_on_flame_path_id'
+  create_table "torches", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "image_url"
+    t.bigint "flame_path_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flame_path_id"], name: "index_torches_on_flame_path_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email'
-    t.string 'fb_id'
-    t.string 'password'
-    t.string 'password_digest'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "fb_id"
+    t.string "password"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'events', 'flame_paths'
-  add_foreign_key 'points', 'flame_paths'
-  add_foreign_key 'torch_votes', 'torches'
-  add_foreign_key 'torch_votes', 'users'
-  add_foreign_key 'torches', 'flame_paths'
+  add_foreign_key "events", "flame_paths"
+  add_foreign_key "points", "flame_paths"
+  add_foreign_key "torch_votes", "torches"
+  add_foreign_key "torch_votes", "users"
+  add_foreign_key "torches", "flame_paths"
 end
